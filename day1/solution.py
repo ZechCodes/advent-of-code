30586ea1dfb28f55379478d0556795887e295c4a
Zech Zimmerman <hi@zech.codes>
2020-12-01 20:30:08 -0500

    Add solution for the second half of the day 1 challenge

--- day1/solution.py
+++ day1/solution.py
@@ -1,4 +1,5 @@
 from itertools import product
+from math import prod
 
 
 def fix_expense_report(report: list[int]) -> int:
@@ -9,9 +10,18 @@ def fix_expense_report(report: list[int]) -> int:
     return -1
 
 
+def product_three_sum_2020(report: list[int]) -> int:
+    for values in product(report, repeat=3):
+        if sum(values) == 2020:
+            return prod(values)
+
+    return -1
+
+
 if __name__ == "__main__":
     import pathlib
 
     with (pathlib.Path(__file__).parent / "input.txt").open() as expense_report:
         expenses = [int(line.strip()) for line in expense_report if line.strip()]
         print(fix_expense_report(expenses))
+        print(product_three_sum_2020(expenses))
